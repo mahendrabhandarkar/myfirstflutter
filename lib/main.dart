db5944337c3ab4e8df7f8a85ff0c5f582e370135
Mahendra Bhandarkar <Mahendra.Bhandarkar@expleogroup.com>
2021-12-29 22:56:09 +0530

    form

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,149 @@
 import 'package:flutter/material.dart';
 
+class FormScreen extends StatefulWidget {
+  const FormScreen({Key? key}) : super(key: key);
+
+  @override
+  _FormScreenState createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  var _firstNameController = TextEditingController();
+  var _lastNameController = TextEditingController();
+  var _phoneController = TextEditingController();
+  var _emailController = TextEditingController();
+
+  String? _gender;
+
+  @override
+  Widget build(BuildContext context) {
+
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Form"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: ListView(
+          children: [
+            TextField(
+              controller: _firstNameController,
+              decoration: InputDecoration(
+                hintText: "First Name",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0)
+                ),
+                prefixIcon: Icon(Icons.supervised_user_circle_rounded),
+              ),
+            ),
+            SizedBox(height: 20.0),
+
+            TextField(
+              controller: _lastNameController,
+              decoration: InputDecoration(
+                hintText: "Last Name",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)
+                ),
+                prefixIcon: Icon(Icons.person),
+              ),
+            ),
+            SizedBox(height: 20.0),
+
+            _genderField(size),
+
+            SizedBox(height: 20.0),
+
+            TextField(
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintText: "Email",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)
+                ),
+                prefixIcon: Icon(Icons.email),
+              ),
+            ),
+            SizedBox(height: 20.0),
+
+            TextField(
+              controller: _phoneController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                hintText: "Phone",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0)
+                ),
+                prefixIcon: Icon(Icons.phone),
+              ),
+            ),
+            SizedBox(height: 20.0),
+
+            ElevatedButton(
+              onPressed: () {
+                print("submit");
+                print(_firstNameController.text);
+                print(_lastNameController.text);
+                print(_phoneController.text);
+                print(_emailController.text);
+                print(_gender);
+              },
+              child: Text("Submit"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _genderField(Size size) {
+    return Row(
+      children: [
+
+        Container(
+          width: (size.width / 2) - 50.0,
+          child: RadioListTile(
+            title: Text("Male"),
+            value: "male",
+            groupValue: _gender,
+            onChanged: (String? str) {
+              print(str);
+              setState(() {
+                _gender = str;
+              });
+
+            },
+          ),
+        ),
+
+        SizedBox(
+            width: 10.0
+        ),
+
+        Container(
+          width: (size.width / 2) - 50.0,
+          child: RadioListTile(
+            title: Text("Female"),
+            value: "female",
+            groupValue: _gender,
+            onChanged: (String? str) {
+              print(str);
+              setState(() {
+                _gender = str;
+              });
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+//            old code base begins from here      
 void main() {
   runApp(const MyApp());
 }
@@ -24,14 +168,15 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Demo Home Page'),
-        ),
-        body: const Center(
-          child: Text('Hello W'),
-        ),
-      ),
+      home: const FormScreen(),
+      // home: Scaffold(
+      //   appBar: AppBar(
+      //     title: const Text('Flutter Demo Home Page'),
+      //   ),
+      //   body: const Center(
+      //     child: Text('Hello W'),
+      //   ),
+      // ),
 
     );
   }
